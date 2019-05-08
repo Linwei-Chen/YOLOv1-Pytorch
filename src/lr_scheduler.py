757d23c731f624fb3135009d984405854b429903
CharlseChen <charleschen2013@163.com>
2019-05-08 16:41:08 +0800

    ...

--- src/lr_scheduler.py
+++ src/lr_scheduler.py
@@ -8,16 +8,16 @@ class WarmUpMultiStepLR(MultiStepLR):
     def __init__(self, optimizer: Optimizer,
                  milestones: List[int],
                  gamma: float = 0.1,
-                 factor: float = 0.1,
-                 num_iters: int = 500,
+                 warm_up_factor: float = 0.1,
+                 warm_up_iters: int = 500,
                  last_epoch: int = -1):
-        self.factor = factor
-        self.num_iters = num_iters
+        self.factor = warm_up_factor
+        self.warm_up_iters = warm_up_iters
         super().__init__(optimizer, milestones, gamma, last_epoch)
 
     def get_lr(self) -> List[float]:
-        if self.last_epoch < self.num_iters:
-            alpha = self.last_epoch / self.num_iters
+        if self.last_epoch < self.warm_up_iters:
+            alpha = self.last_epoch / self.warm_up_iters
             factor = (1 - self.factor) * alpha + self.factor
         else:
             factor = 1
@@ -27,7 +27,7 @@ class WarmUpMultiStepLR(MultiStepLR):
 
 if __name__ == '__main__':
     last_epoch = 2
-    for iter in range(1, 500):
+    for iter in range(1, 1000):
         factor = 0.1
         alpha = iter / 1000
         factor = (1 - factor) * alpha + factor
